e677b01f9ce1d58ebbfba5e6a5f5ee493fbee26a
guverissimo <gustavo.verissimo1@outlook.com>
2025-08-11 17:27:10 -0300

    add ipersonservice

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,15 @@
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+
+        Person Update(Person person);
+        void Delete(long id);
+        List<Person> FindAll();
+
+    }
+}
